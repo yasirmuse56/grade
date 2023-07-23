cd746389aa8e8285bb7ce59968c6e2e44f7091ad
h. mikail <>
2023-07-23 04:35:17 -0700

    gradin  exercise  soluttion

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello");
+
+const grade = 40; 
+if (grade >= 90 && grade <= 100) {
+  console.log("You got an A grade");
+} else if (grade >= 75 && grade < 90) {
+  console.log("You got a B grade");
+} else if (grade >= 60 && grade < 75) {
+  console.log("You got a C grade");
+} 
+else{
+    console.log("you failed")
+}
